16b97d11daa8569a1325284884c101a1e71a5466
shresh <shreshthmehta1@gmail.com>
2024-06-03 21:30:04 +1000

    back

--- src/Tweet.js
+++ src/Tweet.js
@@ -2,61 +2,33 @@ import './App.css';
 import React from 'react';
 import * as Icon from 'react-feather';
 
-// const App = () => {
-//   return <Icon.Camera />
-// };
-
-// export default App;
 function Tweet() {
   return (
     <div className="App">
         <main className="App-main">
-        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">GOLD <a href="https://t.co/9aKqq8R7jC">https://t.co/9aKqq8R7jC</a> via <a href="https://twitter.com/amazon?ref_src=twsrc%5Etfw">@amazon</a></p>&mdash; Shreshth Mehta (@athem108) <a href="https://twitter.com/athem108/status/1672194260458778626?ref_src=twsrc%5Etfw">June 23, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         <br/>
+
+        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">PM <a href="https://twitter.com/narendramodi?ref_src=twsrc%5Etfw">@narendramodi</a> conferred the coveted Prime Minister’s Award for Excellence in Public Administration which was received by J&amp;K Chief Secretary Dr Arun Kumar Mehta, accompanied by CEO Mission Youth JK <a href="https://twitter.com/listenshahid?ref_src=twsrc%5Etfw">@listenshahid</a>, on the occasion of Civil Services Day held at Vigyan Bhawan,… <a href="https://t.co/8i5F7aBay7">pic.twitter.com/8i5F7aBay7</a></p>&mdash; Kashmir Ahead कश्मीर کشمیر (@KashmirAhead) <a href="https://twitter.com/KashmirAhead/status/1649374805467230208?ref_src=twsrc%5Etfw">April 21, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
         <br/>
-        <blockquote class="twitter-tweet"><p lang="en" dir="ltr">Rewind ! All or Nothing: Arsenal <a href="https://t.co/W39HwXite6">https://t.co/W39HwXite6</a> via <a href="https://twitter.com/amazon?ref_src=twsrc%5Etfw">@amazon</a></p>&mdash; Shreshth Mehta (@athem108) <a href="https://twitter.com/athem108/status/1673905299252740096?ref_src=twsrc%5Etfw">June 28, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <blockquote class="twitter-tweet" data-media-max-width="560"><p lang="zxx" dir="ltr"><a href="https://t.co/4zK3iXbAye">pic.twitter.com/4zK3iXbAye</a></p>&mdash; Shreshth Mehta (@athem108) <a href="https://twitter.com/athem108/status/1752547493202870529?ref_src=twsrc%5Etfw">January 31, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<br/>
+
+<blockquote class="twitter-tweet"><p lang="en" dir="ltr">IIT Jammu flagged off 58 students from J&amp;K to Guwahati under <a href="https://twitter.com/EBSB_YuvaSangam?ref_src=twsrc%5Etfw">@EBSB_YuvaSangam</a> by <a href="https://twitter.com/EBSB_Edumin?ref_src=twsrc%5Etfw">@EBSB_Edumin</a> . Chief Secretary Arun Kumar Mehta spoke to students. <a href="https://t.co/1hNKwjzMO6">pic.twitter.com/1hNKwjzMO6</a></p>&mdash; IIT Jammu (@IITJammu) <a href="https://twitter.com/IITJammu/status/1629152013627035648?ref_src=twsrc%5Etfw">February 24, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<blockquote class="twitter-tweet" data-media-max-width="560"><p lang="en" dir="ltr">Lt Governor <a href="https://twitter.com/manojsinha_?ref_src=twsrc%5Etfw">@manojsinha_</a> inaugurates Aerosol Winter School-2023 at Central University, Jammu.<br/>CS, Dr Arun Kumar Mehta also shared his thoughts.<a href="https://twitter.com/OfficeOfLGJandK?ref_src=twsrc%5Etfw">@OfficeOfLGJandK</a> <a href="https://twitter.com/diprjk?ref_src=twsrc%5Etfw">@diprjk</a> @dcjammuofficial <a href="https://twitter.com/OfficialCuj?ref_src=twsrc%5Etfw">@OfficialCuj</a> <a href="https://twitter.com/hashtag/KNS?src=hash&amp;ref_src=twsrc%5Etfw">#KNS</a> <a href="https://twitter.com/hashtag/Kashmir?src=hash&amp;ref_src=twsrc%5Etfw">#Kashmir</a> <a href="https://t.co/qiQPcP6DPW">pic.twitter.com/qiQPcP6DPW</a></p>&mdash; KNS (@KNSKashmir) <a href="https://twitter.com/KNSKashmir/status/1627722068455473155?ref_src=twsrc%5Etfw">February 20, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<br/>
+<blockquote class="twitter-tweet" data-media-max-width="560"><p lang="en" dir="ltr">J&amp;K is developing very much. The UT is not meant for going backward, but for moving forward: Chief Secretary, Dr. Arun Kumar Mehta.<a href="https://twitter.com/OfficeOfLGJandK?ref_src=twsrc%5Etfw">@OfficeOfLGJandK</a> <a href="https://twitter.com/OfficeOfLGJandK?ref_src=twsrc%5Etfw">@OfficeOfLGJandK</a> <a href="https://twitter.com/diprjk?ref_src=twsrc%5Etfw">@diprjk</a> <a href="https://twitter.com/hashtag/KNS?src=hash&amp;ref_src=twsrc%5Etfw">#KNS</a> <a href="https://t.co/VfFlPaUjUi">pic.twitter.com/VfFlPaUjUi</a></p>&mdash; KNS (@KNSKashmir) <a href="https://twitter.com/KNSKashmir/status/1630230217007546368?ref_src=twsrc%5Etfw">February 27, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         <br/>
+        <blockquote class="twitter-tweet" data-media-max-width="560"><p lang="en" dir="ltr"><a href="https://twitter.com/hashtag/Exclusive?src=hash&amp;ref_src=twsrc%5Etfw">#Exclusive</a> CNN-News18: J&amp;K Chief Secretary Arun Kumar Mehta Reflects on the Progress in Kashmir<br/><br/>&#39;There&#39;s been a return to fairness in governance, &amp; the system that we run today is one of the most transparent and fair systems of governance,&#39; he says<a href="https://twitter.com/hashtag/Kashmir?src=hash&amp;ref_src=twsrc%5Etfw">#Kashmir</a> | <a href="https://twitter.com/AnchorAnandN?ref_src=twsrc%5Etfw">@AnchorAnandN</a> <a href="https://t.co/Hz7mwQzuOV">pic.twitter.com/Hz7mwQzuOV</a></p>&mdash; News18 (@CNNnews18) <a href="https://twitter.com/CNNnews18/status/1718689968388710705?ref_src=twsrc%5Etfw">October 29, 2023</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         <br/>
-        <p>“As an Amazon Associate I earn from qualifying purchases.”</p><br/>
-        <p>How do you take your Coffee ?</p>
+<br/>
+        <br/>
         </main>
 
       <footer class="App-footer">
-        
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/shreshth-mehta-5a867361/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <Icon.Linkedin />
-        </a>
-        <a
-          className="App-link"
-          href="https://twitter.com/athem108"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <Icon.Twitter/>
-        </a>
-        <a
-          className="App-link"
-          href="https://github.com/shreshthmehta1"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <Icon.GitHub/>
-        </a>
-
-        <a
-          className="App-link"
-          href="https://www.instagram.com/s.athem/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <Icon.Instagram/>
-        </a>
-
         <a
           className="App-link"
           href="mailto:athem2@icloud.com"
@@ -66,12 +38,9 @@ function Tweet() {
         <Icon.Mail/>
         </a>
         </footer>
-
     </div>
+    
   );
 }
 
 export default Tweet;
-
-
-
